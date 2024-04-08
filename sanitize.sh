b2f4ce038b3165c83bdcf232e51037af9f0508b7
brycebenton <bryce.benton@austintexas.gov>
2024-04-08 10:15:24 -0500

    ddev config

---
+++ sanitize.sh
@@ -0,0 +1,9 @@
+#ddev import-db --file=db.sql.gz
+ddev drush sql-sanitize -y
+ddev drush config:set commerce_payment.commerce_payment_gateway.stripe configuration.publishable_key 'pk_test_dummyValueHere123456789' -y
+ddev drush config:set commerce_payment.commerce_payment_gateway.stripe configuration.secret_key 'sk_test_dummyValueHere123456789' -y
+
+ddev drush config:get commerce_payment.commerce_payment_gateway.stripe configuration.publishable_key\n
+ddev drush config:get commerce_payment.commerce_payment_gateway.stripe configuration.secret_key\n
+ddev drush cr
+ddev launch
